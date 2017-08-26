6ba9c37006149ace161681dd5009430b25f7df4e
frillybob <30709602+frillybob@users.noreply.github.com>
2017-08-26 15:33:59 -0400

    pushy push push

--- app/src/main/java/com/database/SemesterDatabaseHelper.java
+++ app/src/main/java/com/database/SemesterDatabaseHelper.java
@@ -23,14 +23,10 @@ public class SemesterDatabaseHelper extends SQLiteOpenHelper {
                 ClassEntry.COLUMN_SEMESTER + " TIMESTAMP DEFAULT CURRENT_TIMESTAMP" +
                 "); ";
     }
+
     @Override
     public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
-        // For now simply drop the table and create a new one. This means if you change the
-        // DATABASE_VERSION the table will be dropped.
-        // In a production app, this method might be modified to ALTER the table
-        // instead of dropping it, so that existing data is not deleted.
-        // COMPLETED (9) Inside, execute a drop table query, and then call onCreate to re-create it
-        sqLiteDatabase.execSQL("DROP TABLE IF EXISTS " + WaitlistEntry.TABLE_NAME);
+        sqLiteDatabase.execSQL("DROP TABLE IF EXISTS " + ClassEntry.TABLE_NAME);
         onCreate(sqLiteDatabase);
     }
 
