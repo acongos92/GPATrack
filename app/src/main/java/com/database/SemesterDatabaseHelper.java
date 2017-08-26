be545f28f27750078f3c97840acd55f55084e4ca
frillybob <30709602+frillybob@users.noreply.github.com>
2017-08-26 15:25:16 -0400

    onCreate onUpgrade

--- app/src/main/java/com/database/SemesterDatabaseHelper.java
+++ app/src/main/java/com/database/SemesterDatabaseHelper.java
@@ -2,8 +2,9 @@ package com.database;
 
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
 
-public class SemesterDatabaseHelper extends SQLiteOpenHleper {
+public class SemesterDatabaseHelper extends SQLiteOpenHelper {
 
     private static final String DATABASE_NAME = "sem.db";
     private static final int DATABASE_VERSION = 1;
@@ -14,7 +15,22 @@ public class SemesterDatabaseHelper extends SQLiteOpenHleper {
 
     @Override
     public void onCreate(SQLiteDatabase sqLiteDatabase){
-
+        final String SQL_CREATE_WAITLIST_TABLE = "CREATE TABLE " + SemesterDatabase.CLASS_NAME + " (" +
+                WaitlistEntry._ID + " INTEGER PRIMARY KEY AUTOINCREMENT," +
+                WaitlistEntry.COLUMN_GUEST_NAME + " TEXT NOT NULL, " +
+                WaitlistEntry.COLUMN_PARTY_SIZE + " INTEGER NOT NULL, " +
+                WaitlistEntry.COLUMN_TIMESTAMP + " TIMESTAMP DEFAULT CURRENT_TIMESTAMP" +
+                "); ";
+    }
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+        // For now simply drop the table and create a new one. This means if you change the
+        // DATABASE_VERSION the table will be dropped.
+        // In a production app, this method might be modified to ALTER the table
+        // instead of dropping it, so that existing data is not deleted.
+        // COMPLETED (9) Inside, execute a drop table query, and then call onCreate to re-create it
+        sqLiteDatabase.execSQL("DROP TABLE IF EXISTS " + WaitlistEntry.TABLE_NAME);
+        onCreate(sqLiteDatabase);
     }
 
 
