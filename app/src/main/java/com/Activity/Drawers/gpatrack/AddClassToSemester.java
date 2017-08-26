238c5c70ab40e84d63b607290bc19fc898f5083b
acongos92 <30706447+acongos92@users.noreply.github.com>
2017-08-26 14:09:29 -0400

    fix pacakge issue

--- app/src/main/java/com/Activity/Drawers/gpatrack/AddClassToSemester.java
+++ app/src/main/java/com/Activity/Drawers/gpatrack/AddClassToSemester.java
@@ -1,4 +1,4 @@
-package com.Activity.Drawers.gpatrack;
+package com.activity.Drawers.gpatrack;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
--- app/src/main/java/com/Activity/Drawers/gpatrack/SelectSemester.java
+++ app/src/main/java/com/Activity/Drawers/gpatrack/SelectSemester.java
@@ -1,4 +1,4 @@
-package com.Activity.Drawers.gpatrack;
+package com.activity.Drawers.gpatrack;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
