d06bcca4713b9ab0aae92249c9f113c90ac2993e
frillybob <30709602+frillybob@users.noreply.github.com>
2017-08-26 15:22:37 -0400

    asdf

--- app/src/main/java/com/database/SemesterDatabaseHelper.java
+++ app/src/main/java/com/database/SemesterDatabaseHelper.java
@@ -1,9 +1,32 @@
 package com.database;
 
-/**
- * Created by selle on 8/26/2017.
- */
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
 
-public class SemesterDatabaseHelper {
+public class SemesterDatabaseHelper extends SQLiteOpenHleper {
+
+    private static final String DATABASE_NAME = "sem.db";
+    private static final int DATABASE_VERSION = 1;
+
+    public SemesterDatabaseHelper(Context context){
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase){
+
+    }
+
+
+
+
+
+
+
+
+
+
 
 }
+
+
