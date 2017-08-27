3fa4731c1873e6c8ad19191b0fbaf99c6adfdc8d
acongos92 <30706447+acongos92@users.noreply.github.com>
2017-08-27 19:14:14 -0400

    Additional database functionality added

--- app/src/main/java/com/database/SemesterDatabaseQuery.java
+++ app/src/main/java/com/database/SemesterDatabaseQuery.java
@@ -12,7 +12,8 @@ import android.database.sqlite.SQLiteDatabase;
 
 import com.backend_code.DatabaseDTO;
 
-import java.util.ArrayList;
+import java.util.Map;
+import java.util.HashMap;
 import java.util.logging.Logger;
 
 
@@ -21,14 +22,15 @@ public class SemesterDatabaseQuery {
     private static final Logger logger = Logger.getLogger("Database_Logger");
     SQLiteDatabase base;
     SemesterDatabaseHelper dBHelper;
-    public SemesterDatabaseQuery(Context context , Boolean write) {
+
+    public SemesterDatabaseQuery(Context context, Boolean write) {
         logger.info("start semesterDatabaseQuery constructor");
         dBHelper = new SemesterDatabaseHelper(context);
 
         if (write) {
             logger.info("Start writable database constructor");
             base = dBHelper.getWritableDatabase();
-        }else{
+        } else {
             logger.info("Start readable database constructor");
             base = dBHelper.getReadableDatabase();
         }
@@ -37,14 +39,16 @@ public class SemesterDatabaseQuery {
 
     /**
      * adds a particular data transfer object to the database
+     *
      * @param data data transfer object containing data to be removed from the database
      */
-    public void addToDatabase (DatabaseDTO data){
+    public void addToDatabase(DatabaseDTO data) {
+        logger.info("start addToDatabase")
         ContentValues cv = new ContentValues();
 
         cv.put(SemesterDatabase.ClassEntry.COLUMN_CLASS_NAME, data.getClassName());
         cv.put(ClassEntry.COLUMN_GRADE, data.getGrade());
-        cv.put(ClassEntry.COLUMN_SEMESTER,data.getSemester());
+        cv.put(ClassEntry.COLUMN_SEMESTER, data.getSemester());
         cv.put(ClassEntry.COLUMN_CREDIT_HOURS, data.getSemester());
         base.insert(ClassEntry.TABLE_NAME, null, cv);
     }
@@ -53,26 +57,18 @@ public class SemesterDatabaseQuery {
      * removes a particular item from the database, currently uses only class name to determine
      * which column is going to be deleted but should be able to add or remove based on any
      * item in the data DTO
+     *
      * @param data data transfer object containing data to be removed from the database
      */
-    public void removeFromDatabase (DatabaseDTO data) {
-        base.delete(ClassEntry.TABLE_NAME, ClassEntry.COLUMN_CLASS_NAME, new String[] {"=", data.getClassName(), null});
-    }
-
-    //private helpers
-    private Cursor wholeDB() {
-
-        return base.query(ClassEntry.TABLE_NAME, null,null,null,null,null,null,null);
-    }
-
-    private DatabaseDTO buildDTO() {
-        return new DatabaseDTO("wut" ,"wut", 10, 10);
+    public void removeFromDatabase(DatabaseDTO data) {
+        base.delete(ClassEntry.TABLE_NAME, ClassEntry.COLUMN_CLASS_NAME, new String[]{"=", data.getClassName(), null});
     }
 
     /**
      * Returns an array of strings that contain all the current semesters
      */
-    public String[] queryAllSemester(){
+    public String[] queryAllSemester() {
+        logger.info("start queryAllSemesters");
 
         Cursor cursor = wholeDB();
         int numRows = cursor.getCount();
@@ -81,7 +77,7 @@ public class SemesterDatabaseQuery {
         int index = cursor.getColumnIndex(SemesterDatabase.ClassEntry.COLUMN_SEMESTER);
         cursor.moveToFirst();
 
-        for(int i = 0; i < numRows; i++) {
+        for (int i = 0; i < numRows; i++) {
             semesterNames[i] = cursor.getString(index);
             cursor.moveToNext();
         }
@@ -92,10 +88,10 @@ public class SemesterDatabaseQuery {
     }
 
     /**
-     *
-     * @return an integer stack containing all numeric grades
+     * @return an integer array containing all numeric grades
      */
-    public float[] getAllGrades(){
+    public float[] getAllGrades() {
+        logger.info("start getAllGrades");
         Cursor cursor = wholeDB();
         int numRows = cursor.getCount();
 
@@ -103,7 +99,7 @@ public class SemesterDatabaseQuery {
         int index = cursor.getColumnIndex(SemesterDatabase.ClassEntry.COLUMN_GRADE);
         cursor.moveToFirst();
 
-        for(int i = 0; i < numRows; i++) {
+        for (int i = 0; i < numRows; i++) {
             grades[i] = cursor.getFloat(index);
             cursor.moveToNext();
         }
@@ -111,6 +107,67 @@ public class SemesterDatabaseQuery {
         cursor.close();
         return grades;
     }
+    /**
+     * @return a String array containing all class names stored in the database
+     */
+    public String[] getAllClassNames() {
+        logger.info("start getAllClassNames");
+        Cursor cursor = wholeDB();
+        int numRows = cursor.getCount();
+
+        String[] names = new String[numRows];
+        int index = cursor.getColumnIndex(ClassEntry.COLUMN_CLASS_NAME);
+        cursor.moveToFirst();
+
+        for (int i = 0; i < numRows; i++) {
+            names[i] = cursor.getString(index);
+            cursor.moveToNext();
+        }
+
+        cursor.close();
+        return names;
+    }
+
+    /**
+     *
+     * @return map containing all credit hours earned at a particular grade point
+     */
+    public Map<Float, Integer> getCreditHoursAndGrades(){
+        logger.info("start getCreditHoursAndGrades");
+        Cursor cursor = wholeDB();
+        int numRows = cursor.getCount();
+
+        Map<Float, Integer> gradesAndPoints = new HashMap<Float, Integer>();
+        int gradeIndex = cursor.getColumnIndex(ClassEntry.COLUMN_GRADE);
+        int creditIndex = cursor.getColumnIndex(ClassEntry.COLUMN_CREDIT_HOURS);
+
+        cursor.moveToFirst();
+        for(int i = 0; i < numRows; i++) {
+            if(gradesAndPoints.containsKey(cursor.getFloat(gradeIndex))){
+                int current = gradesAndPoints.get(cursor.getFloat(gradeIndex));
+                gradesAndPoints.put(cursor.getFloat(gradeIndex),(current + cursor.getInt(creditIndex)));
+            }else {
+                gradesAndPoints.put(cursor.getFloat(gradeIndex), cursor.getInt(creditIndex));
+            }
+            cursor.moveToNext();
+        }
+
+        return gradesAndPoints;
+    }
+
+
+    //private helpers
+    private Cursor wholeDB() {
+        logger.info("start wholeDB");
+
+        return base.query(ClassEntry.TABLE_NAME, null, null, null, null, null, null, null);
+    }
+
+    private DatabaseDTO buildReturnDTO() {
+        return new DatabaseDTO("wut", "wut", 10, 10);
+    }
+
+
 
 }
 
