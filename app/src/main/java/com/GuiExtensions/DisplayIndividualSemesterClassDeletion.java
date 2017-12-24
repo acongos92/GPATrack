22f9f777c29cf438cdbce7f2ef8a30cc090b9205
acongos92 <30706447+acongos92@users.noreply.github.com>
2017-12-24 18:53:14 -0500

    Refactor individual class deletion
    Move individual class deletion to external class, and refactor a reasonable amount of redundant code (tech debt squashing)

---
+++ app/src/main/java/com/GuiExtensions/DisplayIndividualSemesterClassDeletion.java
@@ -0,0 +1,64 @@
+package com.GuiExtensions;
+
+import android.content.Context;
+import android.content.DialogInterface;
+import android.widget.Toast;
+
+import com.adapters.DisplayIndividualSemesterAdapter;
+import com.backend_code.DatabaseDTO;
+import com.database.SemesterDatabaseQuery;
+import com.gui.DisplayIndivudalSemester;
+
+/**
+ * Created by Andy on 12/24/2017.
+ */
+//probably junk
+public class DisplayIndividualSemesterClassDeletion extends DisplayIndivudalSemester implements DialogInterface.OnClickListener {
+        private String className;
+        private String semName;
+        private Context context;
+        private DisplayIndividualSemesterAdapter displaySemesterAdapter;
+
+        public DisplayIndividualSemesterClassDeletion(Context context, DisplayIndividualSemesterAdapter ad){
+            this.context = context;
+            this.displaySemesterAdapter = ad;
+
+        }
+        @Override
+        public void onClick(DialogInterface dialog, int which){
+            switch (which) {
+                case DialogInterface.BUTTON_POSITIVE:
+                    deleteClass(this.className, this.semName);
+                    makeToast("Class was removed");
+                    dialog.dismiss();
+                    break;
+                case DialogInterface.BUTTON_NEGATIVE:
+                    makeToast("class was not removed");
+                    dialog.dismiss();
+                    break;
+            }
+        }
+        public void setClassName(String className){
+            this.className = className;
+        }
+        public void setSemName(String semName){
+            this.semName = semName;
+        }
+        public void setContext(Context context) {this.context = context;}
+        /*
+         * helpers
+         */
+        private void makeToast(String message){
+            int duration = Toast.LENGTH_SHORT;
+            Toast toast = Toast.makeText(this.context, message, duration);
+            toast.show();
+        }
+    public void deleteClass(String className, String semesterName){
+        SemesterDatabaseQuery SDQWrite = new SemesterDatabaseQuery(this.context, true);
+        DatabaseDTO data = new DatabaseDTO(semesterName, className, 0, 0);
+        SDQWrite.removeFromDatabase(data);
+        displaySemesterAdapter.deleteClassFromAdapter(className);
+        SDQWrite.closeConnection();
+    }
+
+}
--- app/src/main/java/com/gui/DisplayIndivudalSemester.java
+++ app/src/main/java/com/gui/DisplayIndivudalSemester.java
@@ -2,9 +2,7 @@ package com.gui;
 
 import android.app.AlertDialog;
 import android.content.Context;
-import android.content.DialogInterface;
 import android.content.Intent;
-import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
@@ -14,6 +12,7 @@ import android.util.DisplayMetrics;
 import android.view.View;
 import android.widget.Toast;
 
+import com.GuiExtensions.DisplayIndividualSemesterClassDeletion;
 import com.adapters.DisplayIndividualSemesterAdapter;
 import com.backend_code.DatabaseDTO;
 import com.database.SemesterDatabaseQuery;
@@ -31,18 +30,15 @@ public class DisplayIndivudalSemester extends AppCompatActivity implements Displ
 
     private DisplayIndividualSemesterAdapter displaySemesterAdapter;
 
-    private SQLiteDatabase base;
-
     private RecyclerView semesterRecyclerView;
 
     private SemesterDatabaseQuery SDQRead;
-
-    private SemesterDatabaseQuery SDQWrite;
 
     private String SEMESTER_NAME;
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
+        logger.info("Start DiusplayIndividualSemester creation");
         super.onCreate(savedInstanceState);
 
         SDQRead = new SemesterDatabaseQuery(this , false);
@@ -50,11 +46,7 @@ public class DisplayIndivudalSemester extends AppCompatActivity implements Displ
 
         DisplayMetrics dm = new DisplayMetrics();
         getWindowManager().getDefaultDisplay().getMetrics(dm);
-
-
-        Intent intent = getIntent();
-        String semesterName = intent.getExtras().getString("semName");
-
+        String semesterName = getExtras();
         //writing in for proper database deletion method
         this.SEMESTER_NAME = semesterName;
 
@@ -74,9 +66,7 @@ public class DisplayIndivudalSemester extends AppCompatActivity implements Displ
 
             public boolean onMove(RecyclerView recyclerView,
                                            RecyclerView.ViewHolder viewHolder, RecyclerView.ViewHolder target) {
-                final int fromPos = viewHolder.getAdapterPosition();
-                final int toPos = target.getAdapterPosition();
-                // move item in `fromPos` to `toPos` in adapter.
+
                 return true;// true if moved, false otherwise
             }
 
@@ -105,6 +95,22 @@ public class DisplayIndivudalSemester extends AppCompatActivity implements Displ
 
     }
 
+    /*
+     * convenience methods
+     */
+    private String getExtras(){
+        Intent intent = getIntent();
+        String semesterName;
+        if(intent.hasExtra("semName")) {
+            semesterName = intent.getExtras().getString("semName");
+        }
+        else{
+            logger.warning("Semester name not placed in intent, whoops");
+            semesterName = "No Name Found";
+        }
+        return semesterName;
+    }
+
     @Override
     public void onClassItemClick(String semesterItemName){
         //logger.info("DisplayIndividualSemester start onSemesterItemClick");
@@ -116,68 +122,35 @@ public class DisplayIndivudalSemester extends AppCompatActivity implements Displ
 
     private void makeToast(String message){
         Context context = getApplicationContext();
-        CharSequence text = message;
         int duration = Toast.LENGTH_SHORT;
-        Toast toast = Toast.makeText(context, text, duration);
+        Toast toast = Toast.makeText(this, message, duration);
         toast.show();
     }
 
-    private void deleteClass(String className, String semesterName){
-        SDQWrite = new SemesterDatabaseQuery(this, true);
-        DatabaseDTO data = new DatabaseDTO(semesterName, className, 0, 0);
-        SDQWrite.removeFromDatabase(data);
-        displaySemesterAdapter.deleteClassFromAdapter(className);
-        SDQWrite.closeConnection();
-    }
+
     @Override
     public void onDestroy(){
         if(SDQRead != null){
             SDQRead.closeConnection();
         }
-        if(SDQWrite != null){
-            SDQWrite.closeConnection();
-        }
         super.onDestroy();
     }
 
-    public boolean confirmSwipeDelete(String className , String semesterName){
+    public void confirmSwipeDelete(String className , String semesterName){
         String deletePrompt = "Do you wish to delete " + className + "?";
         String yes = "yes";
         String no  ="no";
-        tempListener listener = new tempListener();
+        DisplayIndividualSemesterClassDeletion listener = new DisplayIndividualSemesterClassDeletion(getApplicationContext(), displaySemesterAdapter);
         listener.setClassName(className);
         listener.setSemName(SEMESTER_NAME);
-        new AlertDialog.Builder(this).setIcon(android.R.drawable.ic_dialog_alert).
+        AlertDialog dia = new AlertDialog.Builder(this).setIcon(android.R.drawable.ic_dialog_alert).
                 setTitle("Confirm Delete").setMessage(deletePrompt).setPositiveButton(yes , listener).
-                setNegativeButton("no", listener).show();
+                setNegativeButton(no, listener).show();
 
-        return true;
-    }
-    class tempListener implements DialogInterface.OnClickListener {
-        private String className;
-        private String semName;
-        @Override
-        public void onClick(DialogInterface dialog, int which){
-            switch (which) {
-                case DialogInterface.BUTTON_POSITIVE:
-                    deleteClass(this.className, this.semName);
-                    makeToast("Class was removed");
-                    dialog.dismiss();
-                    break;
-                case DialogInterface.BUTTON_NEGATIVE:
-                    makeToast("class was not removed");
-                    dialog.dismiss();
-                    break;
-            }
-        }
-        public void setClassName(String className){
-            this.className = className;
-        }
-        public void setSemName(String semName){
-            this.semName = semName;
-        }
+
 
     }
+
 
     @Override
     public void onRestart(){
